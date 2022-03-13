3fea2d1caa7ead434aafb77f50f0d0173b4e07bf
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2022-03-13 10:59:16 -0600

    primer commit
    :)

---
+++ Tarea2/Tarea2/Ejercicio1/models/Libreria.cs
@@ -0,0 +1,75 @@
+namespace models
+{
+    internal class libreria
+    {
+      
+		private List<Pelicula> _peliculas;
+		
+
+
+    public void agregar(Pelicula pelicula){
+        if (pelicula != null) {
+                _peliculas.Add(pelicula);
+
+		}
+
+    }
+    public void editar(string nnombre,string nombre,string genero,int año,string director){
+        if (nombre != null&&genero != null&&año != 0 &&director != null) {
+               foreach(Pelicula pelicula in _peliculas)
+                {
+	                if(pelicula.nombre==nombre){
+                        pelicula.genero=genero;
+                        pelicula.año=año;
+                        pelicula.director=director;
+                        pelicula.nombre=nnombre;
+
+                    }
+                }
+
+		}
+
+    }
+
+    public void eliminar(string nombre){
+        if (nombre != null) {
+               foreach(Pelicula pelicula in _peliculas)
+                {
+	                if(pelicula.nombre==nombre){
+                        _peliculas.Remove(pelicula);
+
+                    }
+                }
+
+		}
+
+    }
+    public void mostrar(string nombre){
+        if (nombre != null) {
+               foreach(Pelicula pelicula in _peliculas)
+                {
+	                if(pelicula.nombre==nombre){
+                        Console.WriteLine(pelicula.ToString());
+                        break;
+                    }
+                }
+
+		}
+    }
+    public void mostrarTodos(){
+               foreach(Pelicula pelicula in _peliculas)
+              {
+	               
+                Console.WriteLine(pelicula.ToString());
+                       
+              }
+                
+
+		}
+
+
+    }
+
+		
+	}
+}
---
+++ Tarea2/Tarea2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using models;
+
+Console.WriteLine("Hello, World!");
+Pelicula peli1 = new Pelicula("la marca del zorrillo", "Comedia", 1950,"Gilberto Martínez Solares");
+Console.WriteLine(peli1.ToString());
